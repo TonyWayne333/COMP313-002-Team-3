d5de4027f108c517d3e985cb934414e06d96eda5
Kaps10 <leokaps10@gmail.com>
2020-01-25 22:45:06 -0500

    Some changes made.

--- OnlineAttendanceSystem/app/src/main/java/com/example/onlineattendancesystem/LoginActivity.java
+++ OnlineAttendanceSystem/app/src/main/java/com/example/onlineattendancesystem/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.onlineattendancesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView txtSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtSignup = findViewById(R.id.signup);
+
+        txtSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Prof_Reg_Activity.class));
+            }
+        });
     }
 }
